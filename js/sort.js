b03e0d0b31f0a8fec9019d9a386938b5e3b5d472
kamalov-eldar <kamalov-eldar@mail.ru>
2023-09-05 16:58:04 +0300

    fix

--- js/sort.js
+++ js/sort.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
   $(".catalog__sorter-box").on("click", (event) => {
     const decrease = $(event.currentTarget).hasClass("decrease");
+    console.log("decrease: ", decrease);
 
+    // decrease направление стрелочки
     if (decrease) {
       $(event.currentTarget).removeClass("decrease");
     } else {
@@ -9,13 +11,17 @@ $(document).ready(() => {
     }
 
     const sortField = $(event.currentTarget).data("sort-field");
-
+    //sort-field что сортируем
     const cards = $(".catalog__cards .card");
+    // cards список карточек для сортировки
     const cardsSorted = cards.sort(function (a, b) {
+      // a.dataset[sortField] - значения цена для сортировки
       return decrease
         ? parseInt(a.dataset[sortField]) - parseInt(b.dataset[sortField])
         : parseInt(b.dataset[sortField]) - parseInt(a.dataset[sortField]);
     });
     cardsSorted.appendTo(".catalog__cards");
+   // cardsSorted список дивово карточек отсортированных
+
   });
 });
